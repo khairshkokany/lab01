e7057ce006e97c41ca7d4ba91f0dcc3ff4448510
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-05-23 17:08:05 +0300

    new files

---
+++ class 201/app.js
@@ -0,0 +1,10 @@
+let name=prompt('type ur anime please ..?');
+console.log(name);
+let charc=prompt('type ur character in ur anime ..?');
+console.log(charc);
+let rate=prompt('what is your rating to your anime ..?');
+console.log(rate);
+const userName=prompt('now can u type ur name to save it to me ..?');
+console.log(userName);
+alert('this is your anime '+ name +' and this is your character '+ charc + ' and this is ur rate ' + rate + ' thank you to answer me ..');
+
